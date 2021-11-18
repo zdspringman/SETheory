7934a129220efdeb5031a9a4fc2d8840813caeb5
zdspringman <77950594+zdspringman@users.noreply.github.com>
2021-11-18 13:53:04 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -7,15 +7,6 @@
 			$pass = "password";
 			$user = "brandonharrington";
 			$database = oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpdb');
-
-			if (!$database) {
-				$e = oci_error();
-				trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-			}
-			if ($database) {
-				echo("Connection Successful!");
-			}
-			
         ?>
     </head>
 	<div class="container">
