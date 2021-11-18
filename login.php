c011235e3714a99ca6b2dee0f7f267d4db0df2a9
zdspringman <77950594+zdspringman@users.noreply.github.com>
2021-11-18 13:39:47 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -2,13 +2,19 @@
 	<head>
 		<title>Login</title>
         <link rel="stylesheet" href="style.css">
+        <?php
+            include('username.php');
+			$pass = "password";
+			$user = "brandonharrington";
+			$database = oci_connect($user,$pass,'//csoracle.betheluniversity.edu:1521/orclpdb');
+        ?>
     </head>
 	<div class="container">
 		<div class="form">
 			<label>Login</label>
-			<form>
+			<form method = "post">
 				<label>Username: </label>
-				<input type="text" name="id" required></input>
+				<input type="text" name="username" id = "username" required></input>
 				<input type="submit" value="Log In">
 			</form>
 		</div>
